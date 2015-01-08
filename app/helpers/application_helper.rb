cfe27cf5fe4d4eef2380f0c52f2cebc295de4085
Jake Kaad <kaadalac@gmail.com>
2015-01-08 09:02:09 -0800

    Add fix_date_time helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,5 +4,12 @@ module ApplicationHelper
 		url.starts_with?("http://") ? url : "http://#{url}"
 	end
 
+	def fix_date_time(time)
+		time.strftime("%B %d, '%y at %I:%M%p")
+	end
+
+
+		#2015-01-05 16:24:27.342880
+		#%B %d, '%y at %I:%M%p
 end
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ PostitTemplate::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  Time::DATE_FORMATS[:nice] = "%B %d, '%y at %I:%M%p"
 end
