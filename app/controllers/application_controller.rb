b92f3ad51b71513363b2d02a39733fc4a61a8062
Jake Kaad <kaadalac@gmail.com>
2015-01-15 22:57:58 -0800

    Add simple roles, admin methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :admin?
 
   def logged_in?
   	!!current_user
@@ -13,10 +13,21 @@ class ApplicationController < ActionController::Base
 
   def require_user
     unless logged_in?
-      redirect_to login_path
       flash[:error] = "You must be logged in to do that."
+      redirect_to login_path
     end
   end
+
+  def require_admin
+    access_denied unless logged_in? && current_user.admin?
+  end
+
+  def access_denied
+    flash[:error] = "You can't do that."
+    redirect_to root_path
+  end
+
+
 
 
   protect_from_forgery with: :exception
